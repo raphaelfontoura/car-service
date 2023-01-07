3445321b5a92970f5fed20a0d19d90a20ecb7e45
Raphael Fontoura <raphael.fontoura@gmail.com>
2023-01-07 19:41:41 -0300

    Aula 01

--- src/main/kotlin/br/com/car/adapters/api/CarResource.kt
+++ src/main/kotlin/br/com/car/adapters/api/CarResource.kt
@@ -18,5 +18,9 @@ class CarResource(
 
     @PutMapping("/{id}")
     fun update(@RequestBody car: Car, @PathVariable id: Long) = carService.update(car, id)
+
+    @GetMapping("/inventory")
+    fun listByInventory(@RequestParam model: String) =
+        carService.listByInventory(model)
 }
 
